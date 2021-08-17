549b952303188519c216ed929443248855a3c446
Sergio Bastidas <sergio.batidas@gmail.com>
2021-08-17 18:29:34 -0400

    Primer Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <Navbar />
+    <transition name="vista">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Navbar from "./components/Navbar.vue";
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  components: { Navbar },
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.vista-enter-active,
+.vista-leave-active {
+  transition: opacity 0.1s;
+}
+.vista-enter,
+.vista-leave-to {
+  opacity: 0;
 }
 </style>
---
+++ src/pages/Busquedas.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Busquedas</h1>
+    <input 
+      type="text" 
+      placeholder="Codigo del juego" 
+      :value="$store.state.busquedaPorCodigo" 
+      @input="$store.commit('SET_BUSQUEDA', $event.target.value)"
+    />
+    <ul>
+        <li 
+          v-for="juego in $store.getters.productoFiltrados" 
+          :key="juego.codigo" 
+          :style="{ backgroundColor: juego.color }"
+          > 
+          {{ juego.codigo }} | {{juego.nombre}} | {{juego.precio}} | {{juego.stock}} 
+          </li>
+    </ul>
+    <Listado />
+  </div>
+</template>
+
+<script>
+import Listado from "@/components/Listado.vue";
+export default {
+  name: "BusquedasPage",
+  components: {
+    Listado,
+  },
+};
+</script>
+
+<style></style>
